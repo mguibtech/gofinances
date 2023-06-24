2a180859dc18d8beecb3133f806b08b8274db21d
mguibtech <mguibtech@gmail.com>
2023-06-24 18:30:54 -0400

    feat: Create header component

--- App.tsx
+++ App.tsx
@@ -1,16 +1,69 @@
 
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { Text, View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
 
 import { Dashboard } from './src/screens/Dashboard';
 import theme from './src/global/styles/theme';
 
 export default function App() {
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  const [fontsLoaded] = useFonts({
+
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Poppins_400Regular,
+          Poppins_500Medium,
+          Poppins_700Bold
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <ThemeProvider theme={theme}>
-      <Dashboard/>
-    </ThemeProvider>
-    
+    <View
+      style={{
+        flex: 1
+      }}
+      onLayout={onLayoutRootView}>
+      <ThemeProvider theme={theme}>
+        <Dashboard />
+      </ThemeProvider>
+    </View>
+
+
   );
 }
 
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 
+
 import {
     Container,
-    Title
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    UserWrapper
+
 } from './styles'
 
-export function Dashboard(){
-    return(
+export function Dashboard() {
+    return (
         <Container>
-            <Title>Conteudo</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/112142514?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Guibson</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+
+            </Header>
         </Container>
     )
 }
